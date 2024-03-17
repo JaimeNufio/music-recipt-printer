085cd444e76422d141a3a46d7b58691d5285fb68
Jaime Nufio <jaimeenufio@gmail.com>
2024-03-17 03:22:52 -0400

    printing works

--- recipt.py
+++ recipt.py
@@ -1,12 +1,22 @@
-# from escpos.printer import Usb
+from escpos.printer import Usb
+from PIL import Image 
+
+image = Image.open("a.jpeg")
+
+# Resize the image to 512x512
+resized_image = image.resize((512, 512))
+
+# Save the resized image
+resized_image.save("b.jpg")
 
 # """ Seiko Epson Corp. Receipt Printer (EPSON TM-T88III) """
-# p = Usb(0x04b8, 0x0202, 0, profile="TM-T88III")
-# p.text("-----\n")
-# p.text("12345678901234567890123456789012\n")
-# p.text("----\n")
-# # Cut the paper
-# p.cut()
+p = Usb(0x04b8, 0x0202, 0, profile="TM-T88III")
+p.text("-----\n")
+p.image('b.jpg')
+p.text("12345678901234567890123456789012\n")
+p.text("----\n")
+# Cut the paper
+p.cut()
 
 # # Close the connection
 # p.close()
