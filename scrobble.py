dab0039882d807f30b38079549f84f2591a426d6
Jaime Nufio <jaimeenufio@gmail.com>
2024-03-17 06:29:26 -0400

    cleanup + PIL work

--- scrobble.py
+++ scrobble.py
@@ -5,6 +5,7 @@ from PIL import Image, ImageDraw, ImageFont
 import requests
 from io import BytesIO
 import math
+from recipt import POS_Printer
 
 # Last.fm credentials
 API_KEY = "your_api_key"
@@ -21,12 +22,17 @@ with open('config.json','r') as file:
     PASSWORD = pylast.md5(data['PASSWORD'])
 
 # Authenticate with Last.fm
-network = pylast.LastFMNetwork(api_key=API_KEY, api_secret=API_SECRET, username=USERNAME, password_hash=PASSWORD)
+network = pylast.LastFMNetwork(
+    api_key=API_KEY, 
+    api_secret=API_SECRET, 
+    username=USERNAME, 
+    password_hash=PASSWORD
+    )
 
-# Initialize a variable to keep track of the last scrobbled track
-last_track = None
+last_track = ''
+printer = POS_Printer()
 
-# Function to print the scrobbled track to console
+
 def print_new_scrobble(track):
     global last_track
     if last_track is None or last_track != track:
@@ -35,20 +41,25 @@ def print_new_scrobble(track):
 
 # Function to continuously check for new scrobbles
 def check_for_scrobbles():
+    global last_track
+
     while True:
 
         user = network.get_user(USERNAME)
         now_playing = user.get_now_playing()
 
-        if now_playing:
-
-            artist = now_playing.get_artist().name
-            album = now_playing.get_album().title
-            track = now_playing.title
+        if now_playing and not last_track == now_playing.title:
+            artist = shorten_string(now_playing.get_artist().name)
+            album = shorten_string(now_playing.get_album().title)
+            track = shorten_string(now_playing.title)
             img = now_playing.get_cover_image()
 
-            add_text_to_image(img, album, artist, track, (200, 50) )
-            print(now_playing,'\n',artist,'\n',album,'\n',img,'\n','\n')
+            add_text_to_image(img, album, artist, track )
+            last_track = now_playing.title
+            print("Now Playing: '{}' from '{}' by {}".format(track, album, artist))
+            time.sleep(3)
+        elif now_playing and last_track == now_playing.title:
+            print(".")
             time.sleep(3)
         else:
             print("Nothing Playing")
@@ -59,38 +70,47 @@ def load_image_from_url(url):
     image = Image.open(BytesIO(response.content))
     return image
 
-def add_text_to_image(image_url, album, artist, track, position, font_size=24, font_color=(0,0,0), font_path=None):
+def shorten_string(string, maxSize = 32):
+    if len(string) > maxSize:
+        # Replace the last 2 characters with ...
+        return string[:maxSize-2].strip() + "..."
+    else:
+        return string
 
-    # response = requests.get(image_url)
-    # image = Image.open(BytesIO(response.content))
+def add_text_to_image(image_url, album, artist, track, font_size=22):
+
     icon = load_image_from_url(image_url)
 
-    # Create a blank image with the desired dimensions
     final_width = 512
     final_height = 128
-    background_color = (255, 255, 255)  # White background
+    background_color = (255, 255, 255)  
     final_image = Image.new("RGB", (final_width, final_height), background_color)
 
-    # Resize and paste the icon on the left side with padding
-    padding = 3
+    padding = 10
     icon_size = (final_height - padding*2, final_height - padding*2)
     final_image.paste(icon.resize(icon_size), (padding, padding))
 
-    # Draw text on the right side
     draw = ImageDraw.Draw(final_image)
-    print(len(album))
-    font_scale = 24 #if math.floor(len(album)//1.5) < 24 else math.floor(len(album)//1.5) 
+    font = ImageFont.truetype("verdana.ttf", font_size)
 
-    font = ImageFont.truetype("verdana.ttf", font_scale ) #font_size)
-    text = track # + " - " + artist + "\n" + album
-    text_width, text_height = draw.textsize(text, font=font)
+    track_title = track # + " - " + artist + "\n" + album
+    text_position = (final_height,padding)
+    draw.text(text_position, track_title, fill=(0, 0, 0), font=font)  
 
-    # text_position = ((final_width - text_width) // 2, (final_height - text_height) // 2)  # Center the text vertically
-    
-    text_position = (final_height+padding,padding)
-    draw.text(text_position, text, fill=(0, 0, 0), font=font)  # Fill with black color
+    font = ImageFont.truetype("verdana.ttf", math.floor(font_size*.7))
+    track_title = album
+    text_position = (final_height,padding+(font_size*1.5))
+    draw.text(text_position, track_title, fill=(50, 50, 50), font=font)  
 
+    font = ImageFont.truetype("verdana.ttf", math.floor(font_size*.7))
+    track_title = artist
+    text_position = (final_height,final_height-(font_size*1.5))
+    draw.text(text_position, track_title, fill=(0, 0, 0), font=font)  
+
+    final_image = final_image.convert("L")
     final_image.save('album.jpeg')
+
+    printer.print(path='album.jpeg')
 
 if __name__ == "__main__":
     check_for_scrobbles()
