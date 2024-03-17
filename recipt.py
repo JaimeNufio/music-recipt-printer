85b3643cd9fb24d6b173a76fe9de672faa681574
Jaime Nufio <jaimeenufio@gmail.com>
2024-03-17 06:28:30 -0400

    clean up recipt printer into a library

--- recipt.py
+++ recipt.py
@@ -1,26 +1,13 @@
 from escpos.printer import Usb
-from PIL import Image 
 
-image = Image.open("a.jpeg")
+class POS_Printer:
 
-# Resize the image to 512x512
-resized_image = image.resize((512, 512))
+  def __init__(self):
+    self.printer = Usb(0x04b8, 0x0202, 0, profile="TM-T88III")
+    self.printer.cut()
 
-# Save the resized image
-resized_image.save("b.jpg")
+  def print(self, path):
+    # """ Seiko Epson Corp. Receipt Printer (EPSON TM-T88III) """
+    self.printer.image(path)
+    self.printer.close()
 
-# """ Seiko Epson Corp. Receipt Printer (EPSON TM-T88III) """
-p = Usb(0x04b8, 0x0202, 0, profile="TM-T88III")
-p.text("-----\n")
-p.image('album.jpeg')
-p.text("12345678901234567890123456789012\n")
-p.text("----\n")
-# Cut the paper
-p.cut()
-
-# # Close the connection
-# p.close()
-
-# # p.cut()
-
-# print(len("12345678901234567890123456789012"))
